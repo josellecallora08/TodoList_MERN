4a8f0c3bebcc595c7cc418e944f0eb8ed92382a5
Soj <josellecallora08@gmail.com>
2023-07-26 00:18:01 +0800

    try cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const TaskRoutes = require('./routes/tasks')
 const UserRoutes = require("./routes/users")
-const cors = require('micro-cors');
+const cors = require('cors');
 const mongoose = require('mongoose')
 require('dotenv').config();
 
@@ -10,8 +10,15 @@ require('dotenv').config();
 
 // middleware
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use("/api/task", TaskRoutes);
 app.use("/api/user", UserRoutes)
 
